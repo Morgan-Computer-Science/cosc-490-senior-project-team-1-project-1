3ef1c13a30e1775a540ab07e50d60aae90795160
Abdulai Hallowell <thhal3@morgan.edu>
2026-04-14 11:00:03 -0400

    Use MAMP MySQL socket for local DB connection

--- db.php
+++ db.php
@@ -4,8 +4,10 @@ $host="localhost";
 $user="root";
 $password="root";
 $database="morgan_ai";
+$port=null;
+$socket="/Applications/MAMP/tmp/mysql/mysql.sock";
 
-$conn=new mysqli($host,$user,$password,$database);
+$conn=new mysqli($host,$user,$password,$database,$port,$socket);
 
 if($conn->connect_error){
 die("Connection failed: ".$conn->connect_error);
